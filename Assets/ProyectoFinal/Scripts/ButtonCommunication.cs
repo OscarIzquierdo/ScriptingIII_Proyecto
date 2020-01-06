4433cd0a57557883bc978a4c9f7cc6fdde464721
OscarIzquierdo <oscar.izquierdo@live.u-tad.com>
2020-01-06 18:16:06 +0100

    MainMenu 2.0

--- Assets/ProyectoFinal/Scripts/ButtonCommunication.cs
+++ Assets/ProyectoFinal/Scripts/ButtonCommunication.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonCommunication : MonoBehaviour
 {
+
+    public GameObject optionsPanel;
+    bool isOptionsActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOptionsActive = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateOptions()
     {
-        
+        if (isOptionsActive == false)
+        {
+            optionsPanel.SetActive(true);
+            isOptionsActive = true;
+        }
+    }
+
+    public void DeactivateOptions()
+    {
+        if (isOptionsActive == true)
+        {
+            optionsPanel.SetActive(false);
+            isOptionsActive = false;
+        }
+    }
+
+    public void ChangeScene(string LevelToLoad)
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
