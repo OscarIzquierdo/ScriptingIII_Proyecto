6649adfe29f06d790ff4e1eb6eba91ef27cb5982
swatstar98 <robertitomedusiyo@gmail.com>
2020-01-09 00:14:53 +0100

    Added full animator
    Full working except problems with the animations itself, the rest is fully working,

---
+++ Assets/AttacState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttacState : StateMachineBehaviour
+{
+
+    private Transform m_playerTransform;
+    private UnityEngine.AI.NavMeshAgent m_agent;
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_agent = animator.transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        m_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        m_agent.isStopped = true;
+    }
+
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        float hearingDistance = Vector3.Distance(animator.transform.position, m_playerTransform.position);
+
+        if (hearingDistance > 5)
+        {
+            animator.SetTrigger("Chase");
+        }
+
+        m_agent.SetDestination(m_playerTransform.position);
+
+    }
+}
---
+++ Assets/ChaseState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : StateMachineBehaviour
+{
+
+    private Transform m_playerTransform;
+    private UnityEngine.AI.NavMeshAgent m_agent;
+
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_agent = animator.transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        m_agent.isStopped = false;
+        m_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float hearingDistance = Vector3.Distance(animator.transform.position, m_playerTransform.position);
+
+        if (hearingDistance < 2)
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        if (hearingDistance > 14)
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        m_agent.SetDestination(m_playerTransform.position);
+    }
+
+}
